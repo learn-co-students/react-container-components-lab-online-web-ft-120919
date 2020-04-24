953322282551215bff9373ef0f01d880967a8e24
github username <meliajm@gmail.com>
2020-04-24 14:46:23 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,38 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component {
+
+    state = {
+        reviews: []
+    }
+
+    fetchFromNYTApi = () => {
+        fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({ reviews: data.results.forEach (result => this.state.reviews.push(result.summary_short)) })
+        // this.setState({ reviews: data.results.map( review => ({ review: review.summary_short  })) })
+        // this.setState({ review: data })
+      })
+
+    }
+
+
+
+    componentDidMount() {
+        this.fetchFromNYTApi()
+    }
+
+    render() {
+        return (
+            <div 
+            
+            className='latest-movie-reviews'>
+                hello
+                {this.state.reviews.map((review, id) => <h1 key={id}>{review}</h1>)}
+            </div>
+        )
+    }
+    
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
-// Code MovieReviews Here
+// Code MovieReviews Here --presentation component
+
+import React from 'react'
+
+
+const MovieReviews = () => {
+    return (
+      <div className='review-list'>
+          
+      </div>
+    )
+  }
+
+  export default MovieReviews
