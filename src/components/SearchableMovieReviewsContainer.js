ef5ecb19d1db54c3a5e86283d46fcfd0ea40d590
github username <github email address>
2020-04-28 09:58:44 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,17 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+    constructor(){
+        super()
+        this.state = {
+            reviews: [],
+            searchTerm: ''
+        }
+    }
+
+    
+}
+
+export default SearchableMovieReviewsContainer
