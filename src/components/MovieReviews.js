cb951ba1153a2974ec606ba6d12e4bd34a5c310e
Noah Scala <nwscala@gmail.com>
2020-04-29 15:32:51 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,32 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const Review = ({
+    display_title,
+    headline,
+    byline,
+    summary_short,
+    publication_date,
+    link
+}) => {
+    return (
+        <div key={display_title} className="review">
+            <h3>Title: {display_title}</h3>
+            <h3>Headline: {headline}</h3>
+            <h3>Byline: {byline}</h3>
+            <h4>Summary: {summary_short}</h4>
+            <h5>Publication Date: {publication_date}</h5>
+            <h5> <a href={link.url}> {link.suggested_link_text}</a> </h5>
+            <h3>----------</h3>
+        </div>
+    )
+}
+
+export default function MovieReviews({reviews}) {
+    
+    return (
+        <div className="review-list">
+            {reviews.map(Review)}
+        </div>
+    )
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,37 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        fetch(URL + `&query=${this.state.searchTerm}`)
+            .then(resp => resp.json())
+            .then(reviewData => this.setState({
+                reviews: reviewData.results
+            }))
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange} value={this.state.searchTerm}></input>
+                    <button type="submit">Submit</button>    
+                </form>
+                {this.state.reviews.length > 0 ? <MovieReviews reviews={this.state.reviews} /> : "Nothing Searched Yet"}        
+            </div>
+        )
+    }
+}
